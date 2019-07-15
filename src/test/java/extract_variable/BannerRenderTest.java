19767fbd293bf40182bdb07c8d807a03151553f2
MrLinZHui <742847492@qq.com>
2019-07-15 18:09:18 +0800

    should_return_banner_when_send_platform_contain_mac_and_browser_not_contain_ie

--- src/test/java/extract_variable/BannerRenderTest.java
+++ src/test/java/extract_variable/BannerRenderTest.java
@@ -15,4 +15,22 @@ public class BannerRenderTest {
         //then
         assertEquals("IE on Mac?",renderBanner);
     }
+    @Test
+    public void should_return_banner_when_send_platform_not_contain_mac_and_browser_contain_ie(){
+        //given
+        BannerRender bannerRender = new BannerRender();
+        //when
+        String renderBanner = bannerRender.renderBanner("Ipnoe","browse_ie");
+        //then
+        assertEquals("banner",renderBanner);
+    }
+    @Test
+    public void should_return_banner_when_send_platform_contain_mac_and_browser_not_contain_ie(){
+        //given
+        BannerRender bannerRender = new BannerRender();
+        //when
+        String renderBanner = bannerRender.renderBanner("Ipnoe_mac","browse");
+        //then
+        assertEquals("banner",renderBanner);
+    }
 }
